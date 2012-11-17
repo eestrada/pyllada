96797230ece6cb289b6851c30d2a98b03c05583f
Ethan Estrada <ethan.estrada@gmail.com>
2012-11-16 17:13:47 -0700

    Initial commit

---
+++ src/pyllada.py
@@ -0,0 +1,9 @@
+import xml.etree.ElementTree as ET
+
+class pydae:
+    pass
+
+# Just include a main script to keep testing simple and easy
+if __name__ == '__main__':
+    print("I am testing stuff.")
+
