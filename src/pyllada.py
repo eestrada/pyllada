05239cb7e544519c452b04488b3dee746422d4e8
Ethan Estrada <ethan.estrada@gmail.com>
2012-11-24 10:26:23 -0700

    More work on main class
    Renamed pydae to daedoc to be more descriptive. I think this is going to be less like PyCollada which is more object oriented and mainly a code based interface to a Collada document. Other general improvements were implemented.

--- src/pyllada.py
+++ src/pyllada.py
@@ -40,7 +40,7 @@ else:
 
 __version__ = '0.1'
 
-class pydae(object):
+class daedoc(object):
     __template ='''<?xml version='1.0'?>
     <COLLADA version='1.4.1'>
         <asset>
@@ -65,20 +65,38 @@ class pydae(object):
     </COLLADA>
     '''
 
-    __defaultunits = {'meter': 1.0, 'decimeter': 0.1, 'centimeter': 0.01, \
-            'millimeter': 0.001, 'foot': 0.3048, 'inch': 0.0254 }
+    __defaultunits = {'kilometer': 1000.0, 'meter': 1.0, 'decimeter': 0.1, \
+    'centimeter': 0.01,'millimeter': 0.001, 'mile': 1609.34, 'yard': 0.9144, \
+    'foot': 0.3048, 'inch': 0.0254 }
 
-    def __init__(self, tool=('pyllada ' + __version__)):
+    def __init__(self, tool='', fileobj = None):
         '''Initialize self.'''
-        tmpio = strio(pydae.__template)
-        self.newdoc = True
+        
+        self.newdoc = not fileobj
+        tmpio = strio()
+        tmpattr = '____daedoc_pseudo_attr'
+
+        if not self.newdoc:
+            tmpio = open(str(fileobj), buffering=0, encoding='UTF-8')
+            tmpstr = tmpio.read()
+            tmpstr = tmpstr.replace('xmlns=', tmpattr+'=', 1)
+            tmpio = strio(tmpstr)
+        else:
+            tmpio = strio(daedoc.__template)
+        
         self.tree = xet.parse(tmpio)
         self.root = self.tree.getroot()
-        self.root.attrib['xmlns']='http://www.collada.org/2005/11/COLLADASchema'
+
+        if self.root.attrib.has_key(tmpattr):
+            self.root.attrib['xmlns'] = self.root.attrib[tmpattr]
+            self.root.attrib.pop(tmpattr)
+        else:
+            self.root.attrib['xmlns']='http://www.collada.org/2005/11/COLLADASchema'
+        
         tmpel = self.root.find('asset/contributor/author')
         tmpel.text = str(getpass.getuser())
         tmpel = self.root.find('asset/contributor/authoring_tool')
-        tmpel.text = tool
+        tmpel.text = 'pyllada ' + __version__ + ' | ' + tool
 
 
     def __str__(self):
@@ -100,35 +118,38 @@ class pydae(object):
         usname = str(unit.attrib['name'])
         return tuple(usname, usfloat)
 
-    def setUnitScale(self, scaletype = 'meter'):
-        '''Sets the unit type to have the Collada document use.
+    def setUnitScale(self, scaletype = 'meter', scaleval = None):
+        '''Set the unit type and scale that the Collada document will use.
         
-        scaletype is a string corresponding to the unit size to use. The available options are: meter, decimeter, centimeter, millimeter, foot, inch. Returns True if the type was set. False otherwise.Calling without any parameters will set the units back to the default of meters.'''
-        if isinstance(scaletype, basestring):
-            tmpunit = float()
-            tmptype = str()
+        If called with both scaletype and scaleval, this method will set the scene units to the name of "scaletype" cast as a string, and the meter scale to "scaleval" cast to a float. This allows you to set any unit type with any scaling value, even made up units of measurement (e.g. name="spam-and-eggs" meter="12345.6789").
+        
+        If called with only scaletype, then scaletype is a string corresponding to the unit size to use and the scalevalue will be filled automatically if "scaletype" is a known type. The value of "scaletype" should be a string or something that can be cast to a string. The available options are: "kilometer", "meter", "decimeter", "centimeter", "millimeter", "mile", "yard", "foot", and "inch".
+        
+        Calling without any parameters will set scene unit scale back to the default of meters.
+
+        Returns True if the type was set. False otherwise.'''
+
+        tmpel = self.root.find('asset/unit')
+        if(scaleval != None):
+            tmpel.attrib = {'name': str(scaletype), 'meter':str(float(scaleval))}
+        else:
+            tmpunit = str()
+            tmptype = str(scaletype).lower()
             try:
-                tmpunit = float(__defaultunits[scaletype])
-                tmptype = str(scaletype)
+                tmpunit = str(daedoc.__defaultunits[tmptype])
             except KeyError:
                 return False
+            tmpel.attrib = {'name': tmptype, 'meter':tmpunit}
 
-            self.root.find('asset/unit').attrib = {'name': tmptype, 'meter':tmpunit}
-            return True
-
-        return False
+        return True
 
     def setUpAxis(self, upaxis='Y_UP'):
         '''Set the up axis of the scene.
         
-        Defaults to Y up. The input is a string with a value of: "x", "y", "z", "X_UP", "Y_UP", or "Z_UP". This method is case insensitive. Returns true on successful change. False otherwise.'''
-
-        # Return false if this isn't a string
-        if not isinstance(upaxis, basestring):
-            return False
+        Defaults to Y up. The input is a string with a value of: "x", "y", "z", "X_UP", "Y_UP", or "Z_UP". This method is case insensitive. Returns True on successful change. False otherwise.'''
 
         axisdict = {'x':'X_UP', 'y':'Y_UP', 'z':'Z_UP'}
-        tmpkey = upaxis.lower[0]
+        tmpkey = str(upaxis).lower()[0]
         tmpel = self.root.find('asset/up_axis')
         
         try:
@@ -137,12 +158,17 @@ class pydae(object):
         except KeyError:
             return False
 
+        return True
+
     def addGeometry(self):
         '''Add to geometry library. Not implemented yet.'''
         return NotImplemented
 
-    def indent(self, elem, level=0, white='  '):
-        '''Recursive method to create pleasing indentation in Collada docs.'''
+    def _indent(self, elem, white='\t\t', level=0):
+        '''Recursive method to create pleasing indentation in Collada docs.
+        
+        Called by default when writing file to disk or generating a string representation of the document.'''
+
         i = '\n' + level * white
         if len(elem):
             if not elem.text or not elem.text.strip():
@@ -150,27 +176,46 @@ class pydae(object):
             if not elem.tail or not elem.tail.strip():
                 elem.tail = i
             for elem in elem:
-                self.indent(elem, level+1)
+                self._indent(elem, level = level+1)
             if not elem.tail or not elem.tail.strip():
                 elem.tail = i
         else:
             if level and (not elem.tail or not elem.tail.strip()):
                 elem.tail = i
     
-    def write(self, fobj=sys.stdout, encoding='UTF-8'):
-        '''Write collada document to the file specified by "fobj".
-
-        fobj can be either a file name or a file like object open for writing. encoding is the encoding of the output (default is UTF-8). Encoding does not generally need to be changed.'''
-        now = datetime.isoformat(datetime.now())
+    def _flatten(self, elem):
+        '''Recursive method to remove unnecessary white space from the document.
         
+        This can make documents slightly smaller on disk with the downside that human readability is reduced. However, the document will still be parsable by machine.
+        
+        The savings in disk spaced gained by flattening the document are usually very minimal, so using this method is discouraged.'''
+
+        if isinstance(elem.text, basestring):
+            elem.text = elem.text.strip()
+        if isinstance(elem.tail, basestring):
+            elem.tail = elem.tail.strip()
+        for elem in elem:
+            self._flatten(elem)
+    
+
+    def write(self, fileobj=sys.stdout, encoding='UTF-8', indent=True):
+        '''Write collada document to the file specified by "fileobj".
+
+        fileobj can be either a file name or a file like object open for writing. encoding is the encoding of the output (default is UTF-8). Encoding does not generally need to be changed.'''
+        
+        now = datetime.isoformat(datetime.now())
         if self.newdoc:
             tmpel = self.root.find('asset/created')
             tmpel.text = now
-        
         tmpel = self.root.find('asset/modified')
         tmpel.text = now
-        self.indent(self.root)
-        self.tree.write(fobj, encoding)
+        
+        if indent:
+            self._indent(self.root)
+        else:
+            self._flatten(self.root)
+
+        self.tree.write(fileobj, encoding)
 
 # Just include a main script to keep testing simple and easy
 if __name__ == '__main__':
@@ -178,18 +223,15 @@ if __name__ == '__main__':
     
     print('I am testing stuff.')
 
-    testdae = pydae()
+    testdae = daedoc(fileobj = '/tmp/test.dae')
 
 #    testdae.test()
 
-#    testdae.write(sys.stdout)
+    testdae.setUnitScale('Mile')
 
-    stest = 'What am I?'
+#    print(help(daedoc.setUnitScale))
 
-    print(type(stest))
-
-    print(xet)
-    print(strio)
-
-    print(testdae)
-
+    testdae.write('/tmp/testflat.dae', indent=False)
+    testdae.write('/tmp/testindent.dae', indent=True)
+    
+    print(testdae.newdoc)
